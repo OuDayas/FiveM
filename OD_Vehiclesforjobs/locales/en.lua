0de45408aa9ff0564762ea2cd20115c71f9cccc0
OuDayas <carmine.giova@gmail.com>
2023-02-27 21:22:18 +0100

    Delete OD_Vehiclesforjobs directory

--- OD_Vehiclesforjobs/locales/en.lua
+++
@@ -1,7 +0,0 @@
-Locales['en'] = {
-  -- Cloakroom
-  ['takeveh'] = 'Press ~INPUT_PICKUP~ for take a vehicle',
-  ['GarageOccupied'] = 'Something obstruct the passage for the garage',
-  ['InputPickup'] = 'Press ~INPUT_PICKUP~ to park it',
-  ['TakeVeh'] = 'Press ~INPUT_PICKUP~ to take that vehicle',
-}
--- OD_Vehiclesforjobs/locales/it.lua
+++
@@ -1,7 +0,0 @@
-Locales['it'] = {
-  -- Cloakroom
-  ['takeveh'] = 'Premi ~INPUT_PICKUP~ per prendere un veicolo',
-  ['GarageOccupied'] = 'Un veicolo ostruisce il passaggio',
-  ['InputPickup'] = 'Premi ~INPUT_PICKUP~ per parcheggiare',
-  ['TakeVeh'] = 'Premi ~INPUT_PICKUP~ per prendere questo veicolo',
-}
